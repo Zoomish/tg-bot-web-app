16c525a4ef5e35d9f401a9139de6bc3b0d76b71e
Zoom <egor147536987@gmail.com>
2023-12-20 21:45:08 +0300

    initials

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { useEffect } from 'react';
 import './App.css';
-const tg=window.Telegram.WebApp
+
 function App() {
+
   useEffect(()=>{
     tg.ready();
   }, [])
 
-  const onClose=()=>{
-    tg.close()
-  }
   return (
     <div className="App">
-      <button onClick={onClose}>Закрыть</button>
+      work
     </div>
   );
 }
