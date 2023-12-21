169e5e4e282b2f4590f255a2613a168992e7011c
Zoom <egor147536987@gmail.com>
2023-12-21 20:29:56 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,48 +15,47 @@ const Form = () => {
         })
     },[])
 
-    useEffect(()=>{
-        if (!street || !country) {
+    useEffect(() => {
+        if(!street || !country) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
         }
-    },[country, street])
+    }, [country, street])
 
-    const onChangeCountry=(e)=>{
+    const onChangeCountry = (e) => {
         setCountry(e.target.value)
     }
-    const onChangeStreet=(e)=>{
+
+    const onChangeStreet = (e) => {
         setStreet(e.target.value)
     }
-    const onChangeSubject=(e)=>{
+
+    const onChangeSubject = (e) => {
         setSubject(e.target.value)
     }
 
-
     return (
-        <div className='form'>
+        <div className={"form"}>
             <h3>Введите ваши данные</h3>
-            <input 
-                className='input' 
-                type="text" 
-                placeholder='Страна' 
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Страна'}
                 value={country}
                 onChange={onChangeCountry}
             />
-            <input 
-                className='input' 
-                type="text" 
-                placeholder='Улица' 
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Улица'}
                 value={street}
                 onChange={onChangeStreet}
             />
             <select value={subject} onChange={onChangeSubject} className={'select'}>
-                <option value="physical">Юр. лицо</option>
-                <option value="legal">Физ. лицо</option>
+                <option value={'physical'}>Физ. лицо</option>
+                <option value={'legal'}>Юр. лицо</option>
             </select>
-
-
         </div>
     );
 };
