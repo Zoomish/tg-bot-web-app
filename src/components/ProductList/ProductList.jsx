3372a230e7c65b62b3cc5cf6128fbfbd6c27143b
Zoom <egor147536987@gmail.com>
2023-12-22 17:41:35 +0300

    initial

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -14,39 +14,11 @@ const products = [
     {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
 ]
 
-const getTotalPrice=(items=[])=>{
-    return items.reduce((acc,item)=>{
-        return acc+=item.price
-    },0)
-}
 
 const ProductList = () => {
     const [addedItems, setAddedItems]=useState([]);
     const {tg, queryId}=useTelegram();
-    const onSendData = useCallback(() => {
-        const data = {
-            products:addedItems,
-            totalPrice:getTotalPrice(addedItems),
-            queryId,
-
-        }
-        fetch('http://localhost:3000', {
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        })
-    }, [])
-
-
-    useEffect(() => {
-        tg.onEvent('mainButtonClicked', onSendData)
-        return () => {
-            tg.offEvent('mainButtonClicked', onSendData)
-        }
-    }, [onSendData])
-
+    
 
 
     const onAdd=(product)=>{
